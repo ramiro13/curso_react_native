e5d8c8ff220bdb280f78479430402d216594e909
rami13kt <ramiroperezsanz13@gmail.com>
2022-02-10 07:10:36 +0100

    update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola estoy en tu device</Text>
+      <Text>Hola estoy en tu dispositivo </Text>
       <StatusBar style="auto" />
     </View>
   );
